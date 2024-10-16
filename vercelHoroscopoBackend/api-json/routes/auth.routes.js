e7c1d818e62b06f04620b43751419a37d38aefee
Manolinhopng <145302101+Manolinhopng@users.noreply.github.com>
2024-10-15 22:40:23 -0500

    Delete vercelHoroscopoBackend/api-json directory

--- vercelHoroscopoBackend/api-json/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const signoController = require("./controllers/signoController");
-
-router.post("/login", signoController.loginUser);
-
-module.exports = router;
